3d8253acd59ec3094e8bd3340f6da276c62e9fdd
Ekin Koluk <ekinkoluk@gmail.com>
2021-11-08 04:01:32 +0000

    added move commands

--- play.js
+++ play.js
@@ -3,10 +3,10 @@ const {setupInput} = require("./input");
 
 console.log("Connecting ...");
 
-connect();
+
 
 // setup interface to handle user input from stdin
 
 
-setupInput();
+setupInput(connect());
 
